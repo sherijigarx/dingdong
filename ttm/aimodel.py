77c0e63bd0a055e995d51e27300490870fbc236d
sherijigarx <155580193+sherijigarx@users.noreply.github.com>
2024-10-04 00:42:12 +0500

    Update aimodel.py

--- ttm/aimodel.py
+++ ttm/aimodel.py
@@ -35,7 +35,7 @@ class AIModelService:
             AIModelService._base_initialized = True
 
         if AIModelService._scores is None:
-            AIModelService._scores = self.metagraph.E.copy()
+            AIModelService._scores = self.metagraph.E.clone().detach()
         self.scores = AIModelService._scores
         self.uid = self.metagraph.hotkeys.index(self.wallet.hotkey.ss58_address)
 
